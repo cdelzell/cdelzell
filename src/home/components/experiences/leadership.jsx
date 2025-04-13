aa06919c02cb002a7412039514b7801e4f74cd4c
cdelzell <claradelzell@gmail.com>
2025-04-13 09:09:52 -0500

    leadership updates

--- src/home/components/experiences/leadership.jsx
+++ src/home/components/experiences/leadership.jsx
@@ -26,15 +26,15 @@ function Leadership() {
         <Experience
           image={salt}
           name="SALT Ministry: Student Leader"
-          blurb="One highlight of this experience was getting to learn more about the database world as we worked on moving from flat to relational data!"
+          blurb="Getting to see the girls in our group grow has been a beautiful experience, and has also taught me how to build community from the ground up."
           details="co-lead a group of 10-15 girls weekly, assist with Welcome team at weekly 500+ gatherings."
-          time="January 2024 - May 2024"
+          time="August 2024 - Present"
         />
 
         <Experience
           image={raikes}
           name="Kauffman Residents Assn Vice President"
-          blurb="Initially, I joined this because I was frustrated with the state of our kitchens. We were able to fully restock our kitchens as of December 2024, with new items, even getting a Kitchen-Aid!"
+          blurb="Initially, I joined this because I was frustrated with the state of our dorm kitchens. We were able to revamp our kitchens as of December 2024 with new items, even getting a Kitchen-Aid!"
           details="work on the executive team to keep the building properly maintained and organize events for residents."
           time="August 2024 - Present"
         />
@@ -42,7 +42,7 @@ function Leadership() {
         <Experience
           image={nhri}
           name="NHRI Leadership Mentor"
-          blurb="I helped with this class in the fall of 2023, and am returning for the fall of 2024! This class is a great way to teach entreprenuership, and has been a highlight of my college career."
+          blurb="Mentorship has had a huge impact in my own life, and this was an opportunity to pass that on to someone else."
           details="selected as a mentor in the NHRI class of 2023. meet weekly with a high school mentee and attend weekly project meetings."
           time="March 2024 - Present"
         />
